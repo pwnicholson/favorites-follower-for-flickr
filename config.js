986ffc8eb54ceadc562a16985093921c89cc0c97
Paul Nicholson <271459+pwnicholson@users.noreply.github.com>
2026-07-31 14:45:42 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+// Local Configuration for API Credentials
+// Keep this file in .gitignore for public repos.
+// Build step for Web Store compilation will populate these fields directly.
+const CONFIG = {
+  FLICKR_API_KEY: "",
+  FLICKR_API_SECRET: ""
+};
+
+export default CONFIG;
